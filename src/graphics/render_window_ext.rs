24d133fbb6a4e0b19aef103f5f6694801541fc1f
crumblingstatue <radiantstatue@gmail.com>
2023-07-18 20:15:42 +0200

    Add RenderWindowExt::desktop_fullscreen

--- src/graphics/render_window_ext.rs
+++ src/graphics/render_window_ext.rs
@@ -1,13 +1,15 @@
 use sfml::{
     graphics::{RenderTarget, RenderWindow},
     system::Vector2,
-    window::VideoMode,
+    window::{ContextSettings, Style, VideoMode},
 };
 
 /// Extensions for `RenderWindow`
 pub trait RenderWindowExt {
     /// Center the window on screen
     fn center(&mut self);
+    /// Enter a fake fullscreen mode (desktop fullscreen)
+    fn desktop_fullscreen(&mut self, title: &str, ctx_settings: &ContextSettings);
 }
 
 impl RenderWindowExt for RenderWindow {
@@ -19,5 +21,10 @@ impl RenderWindowExt for RenderWindow {
         let new_x = width_diff / 2;
         let new_y = height_diff / 2;
         self.set_position(Vector2::new(new_x as i32, new_y as i32));
+    }
+    fn desktop_fullscreen(&mut self, title: &str, ctx_settings: &ContextSettings) {
+        let desktop_mode = VideoMode::desktop_mode();
+        self.recreate(desktop_mode, title, Style::NONE, ctx_settings);
+        self.set_position((0, 0).into());
     }
 }
