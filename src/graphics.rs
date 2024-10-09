a30ff49c94416c436e65944dbf72f73e1f2aecce
crumblingstatue <radiantstatue@gmail.com>
2024-10-09 20:17:48 +0200

    Add VertexVecExt extension trait

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,2 +1,4 @@
 mod render_window_ext;
+mod vertex_vec_ext;
 pub use render_window_ext::RenderWindowExt;
+pub use vertex_vec_ext::VertexVecExt;
---
+++ src/graphics/vertex_vec_ext.rs
@@ -0,0 +1,49 @@
+use sfml::{
+    graphics::{Color, FloatRect, IntRect, Vertex},
+    system::Vector2,
+};
+
+/// Convenience methods for a `Vec` of vertices (vertex array).
+pub trait VertexVecExt {
+    /// Push a quad to a vector of quad primitives
+    fn push_quad(&mut self, dst_rect: FloatRect, texture_rect: IntRect, color: Color);
+}
+
+impl VertexVecExt for Vec<Vertex> {
+    fn push_quad(&mut self, dst_rect: FloatRect, texture_rect: IntRect, color: Color) {
+        self.extend_from_slice(&[
+            Vertex {
+                position: Vector2::new(dst_rect.left, dst_rect.top),
+                color,
+                tex_coords: Vector2::new(texture_rect.left as f32, texture_rect.top as f32),
+            },
+            Vertex {
+                position: Vector2::new(dst_rect.left + dst_rect.width, dst_rect.top),
+                color,
+                tex_coords: Vector2::new(
+                    (texture_rect.left + texture_rect.width) as f32,
+                    texture_rect.top as f32,
+                ),
+            },
+            Vertex {
+                position: Vector2::new(
+                    dst_rect.left + dst_rect.width,
+                    dst_rect.top + dst_rect.height,
+                ),
+                color,
+                tex_coords: Vector2::new(
+                    (texture_rect.left + texture_rect.width) as f32,
+                    (texture_rect.top + texture_rect.height) as f32,
+                ),
+            },
+            Vertex {
+                position: Vector2::new(dst_rect.left, dst_rect.top + dst_rect.height),
+                color,
+                tex_coords: Vector2::new(
+                    texture_rect.left as f32,
+                    (texture_rect.top + texture_rect.height) as f32,
+                ),
+            },
+        ]);
+    }
+}
